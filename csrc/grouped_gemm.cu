716d387020d6a45e7a995e9d917bd2ecc7a4c580
路加 <lujia.lmj@alibaba-inc.com>
2024-11-16 20:16:53 +0800

    use hipblaslt

--- csrc/grouped_gemm.cu
+++ csrc/grouped_gemm.cu
@@ -5,6 +5,9 @@
 #include <c10/util/BFloat16.h>
 #include <torch/extension.h>
 
+#include <hipblaslt/hipblaslt-ext.hpp>
+#include <hipblaslt/hipblaslt.h>
+
 namespace grouped_gemm {
 
 #define NUM_STREAM 4
@@ -38,7 +41,7 @@ torch::Tensor CopyToDevice(const std::vector<T> &x,
   return out;
 }
 
-cublasHandle_t cublas_handle[NUM_STREAM];
+hipblasLtHandle_t cublas_handle[NUM_STREAM];
 cudaStream_t cublas_stream[NUM_STREAM];
 cudaEvent_t cublas_event[NUM_STREAM];
 bool cublas_init = false;
@@ -48,8 +51,7 @@ void cublas_handle_init() {
 
   for (int i = 0; i < NUM_STREAM; i++) {
     cudaStreamCreateWithFlags(&cublas_stream[i], cudaStreamNonBlocking);
-    cublasCreate(&cublas_handle[i]);
-    cublasSetStream(cublas_handle[i], cublas_stream[i]);
+    hipblasLtCreate(&cublas_handle[i]);
     cudaEventCreate(&cublas_event[i]);
   }
 }
@@ -72,10 +74,114 @@ inline void cublas_streams_wait_current(cudaStream_t stream) {
   }
 }
 
+void gemmex_wrapper_bf16(hipblasHandle_t handle, hipblasOperation_t transa,
+                         hipblasOperation_t transb, int m, int n, int k,
+                         int batch_count, float &alpha, float &beta,
+                         c10::BFloat16 *A, c10::BFloat16 *B, c10::BFloat16 *C,
+                         c10::BFloat16 *D, void *d_workspace,
+                         int64_t max_workspace_size, hipStream_t stream) {
+  hipblasLtMatrixLayout_t matA, matB, matC, matD;
+  CHECK_HIPBLASLT_ERROR(
+      hipblasLtMatrixLayoutCreate(&matA, HIP_R_16BF, m, k, m));
+  CHECK_HIPBLASLT_ERROR(
+      hipblasLtMatrixLayoutCreate(&matB, HIP_R_16BF, n, k, n));
+  CHECK_HIPBLASLT_ERROR(
+      hipblasLtMatrixLayoutCreate(&matC, HIP_R_16BF, m, n, m));
+  CHECK_HIPBLASLT_ERROR(
+      hipblasLtMatrixLayoutCreate(&matD, HIP_R_16BF, m, n, m));
+
+  if (batch_count > 1) {
+    int64_t stride_a = m * k;
+    int64_t stride_b = k * n;
+    int64_t stride_c = m * n;
+    int64_t stride_d = m * n;
+    CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutSetAttribute(
+        matA, HIPBLASLT_MATRIX_LAYOUT_BATCH_COUNT, &batch_count,
+        sizeof(batch_count)));
+    CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutSetAttribute(
+        matA, HIPBLASLT_MATRIX_LAYOUT_STRIDED_BATCH_OFFSET, &stride_a,
+        sizeof(stride_a)));
+    CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutSetAttribute(
+        matB, HIPBLASLT_MATRIX_LAYOUT_BATCH_COUNT, &batch_count,
+        sizeof(batch_count)));
+    CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutSetAttribute(
+        matB, HIPBLASLT_MATRIX_LAYOUT_STRIDED_BATCH_OFFSET, &stride_b,
+        sizeof(stride_b)));
+    CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutSetAttribute(
+        matC, HIPBLASLT_MATRIX_LAYOUT_BATCH_COUNT, &batch_count,
+        sizeof(batch_count)));
+    CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutSetAttribute(
+        matC, HIPBLASLT_MATRIX_LAYOUT_STRIDED_BATCH_OFFSET, &stride_c,
+        sizeof(stride_c)));
+    CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutSetAttribute(
+        matD, HIPBLASLT_MATRIX_LAYOUT_BATCH_COUNT, &batch_count,
+        sizeof(batch_count)));
+    CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutSetAttribute(
+        matD, HIPBLASLT_MATRIX_LAYOUT_STRIDED_BATCH_OFFSET, &stride_d,
+        sizeof(stride_d)));
+  }
+
+  hipblasLtMatmulDesc_t matmul;
+  CHECK_HIPBLASLT_ERROR(
+      hipblasLtMatmulDescCreate(&matmul, HIPBLAS_COMPUTE_32F, HIP_R_32F));
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatmulDescSetAttribute(
+      matmul, HIPBLASLT_MATMUL_DESC_TRANSA, &transa, sizeof(int32_t)));
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatmulDescSetAttribute(
+      matmul, HIPBLASLT_MATMUL_DESC_TRANSB, &transb, sizeof(int32_t)));
+
+  hipblasLtEpilogue_t epilogue = HIPBLASLT_EPILOGUE_DEFAULT;
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatmulDescSetAttribute(
+      matmul, HIPBLASLT_MATMUL_DESC_EPILOGUE, &epilogue, sizeof(epilogue)));
+
+  // Set User Preference attributes
+  hipblasLtMatmulPreference_t pref;
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatmulPreferenceCreate(&pref));
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatmulPreferenceSetAttribute(
+      pref, HIPBLASLT_MATMUL_PREF_MAX_WORKSPACE_BYTES, &max_workspace_size,
+      sizeof(max_workspace_size)));
+
+  const int request_solutions = 1;
+  hipblasLtMatmulHeuristicResult_t heuristicResult[request_solutions];
+  int returnedAlgoCount = 0;
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatmulAlgoGetHeuristic(
+      handle, matmul, matA, matB, matC, matD, pref, request_solutions,
+      heuristicResult, &returnedAlgoCount));
+
+  if (returnedAlgoCount == 0) {
+    std::cerr << "No valid solution found!" << std::endl;
+    return;
+  }
+
+  uint64_t workspace_size = 0;
+  for (int i = 0; i < returnedAlgoCount; i++)
+    workspace_size = max(workspace_size, heuristicResult[i].workspaceSize);
+  // In this sample, the workspace is already allocated with max_workspace_size
+  // If not, allocate d_workspace here
+  // CHECK_HIP_ERRORhipMalloc(&d_workspace, workspace_size));
+
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatmul(
+      handle, matmul, &alpha, A, matA, B, matB, &beta, C, matC, D, matD,
+      &heuristicResult[0].algo, d_workspace, workspace_size, stream));
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutDestroy(matA));
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutDestroy(matB));
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutDestroy(matC));
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatrixLayoutDestroy(matD));
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatmulDescDestroy(matmul));
+  CHECK_HIPBLASLT_ERROR(hipblasLtMatmulPreferenceDestroy(pref));
+  return;
+}
+
+// void gemmex_wrapper_bf16(hipblasHandle_t handle, hipblasOperation_t transa,
+//                          hipblasOperation_t transb, int m, int n, int k,
+//                          int batch_count, float &alpha, float &beta,
+//                          c10::BFloat16 *A, c10::BFloat16 *B, c10::BFloat16
+//                          *C, c10::BFloat16 *D, void *d_workspace, int64_t
+//                          max_workspace_size, hipStream_t stream);
+
 void CublasGemm(cublasHandle_t cublas_handle, c10::BFloat16 *a, int64_t a_rows,
                 int64_t a_cols, bool trans_a, c10::BFloat16 *b, int64_t b_rows,
                 int64_t b_cols, bool trans_b, c10::BFloat16 *c, int64_t c_rows,
-                int64_t c_cols) {
+                int64_t c_cols, cublasStream_t cublas_stream) {
   int m = trans_b ? b_rows : b_cols;
   int k = trans_b ? b_cols : b_rows;
   int n = trans_a ? a_cols : a_rows;
@@ -86,10 +192,20 @@ void CublasGemm(cublasHandle_t cublas_handle, c10::BFloat16 *a, int64_t a_rows,
   cublasOperation_t transpose_b = trans_b ? CUBLAS_OP_T : CUBLAS_OP_N;
 
   float alpha = 1.0, beta = 0.0;
-  CUBLAS_CALL(cublasGemmEx(cublas_handle, transpose_b, transpose_a, m, n, k,
-                           &alpha, b, CUDA_R_16BF, ldb, a, CUDA_R_16BF, lda,
-                           &beta, c, CUDA_R_16BF, c_cols, HIPBLAS_COMPUTE_32F,
-                           CUBLAS_GEMM_DEFAULT));
+  const int batch_count = 0;
+  int64_t max_workspace_size = 32 * 1024 * 1024;
+  void *d_workspace;
+  if (max_workspace_size > 0)
+    CHECK_HIP_ERROR(hipMalloc(&d_workspace, max_workspace_size));
+
+  gemmex_wrapper_bf16(cublas_handle, transpose_a, transpose_b, m, n, k,
+                      batch_count, alpha, beta, a, b, c, c, d_workspace,
+                      max_workspace_size, cublas_stream);
+
+  // CUBLAS_CALL(cublasGemmEx(cublas_handle, transpose_b, transpose_a, m, n, k,
+  //                          &alpha, b, CUDA_R_16BF, ldb, a, CUDA_R_16BF, lda,
+  //                          &beta, c, CUDA_R_16BF, c_cols,
+  //                          HIPBLAS_COMPUTE_32F, CUBLAS_GEMM_DEFAULT));
 }
 
 void CublasGroupedGemm(torch::Tensor a, torch::Tensor b, torch::Tensor c,
@@ -110,7 +226,8 @@ void CublasGroupedGemm(torch::Tensor a, torch::Tensor b, torch::Tensor c,
 
     int64_t m = batch_sizes.data_ptr<int64_t>()[i];
     CublasGemm(cublas_handle[i % NUM_STREAM], a_ptr, m, k, /*trans_a=*/false,
-               b_ptr, b_rows, b_cols, trans_b, c_ptr, m, n);
+               b_ptr, b_rows, b_cols, trans_b, c_ptr, m, n,
+               cublas_stream[i % NUM_STREAM]);
     a_ptr += m * k;
     b_ptr += b_rows * b_cols;
     c_ptr += m * n;
